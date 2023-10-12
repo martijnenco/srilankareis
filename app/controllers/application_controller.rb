5503fbacc57d2bea919d34936188a65e71c5cba4
Martijn <m-j-heiden@hotmail.com>
2023-10-12 17:16:20 +0200

    Add landing and theme page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_pagetitle
+
+  private
+  
+  def set_pagetitle
+    @pagetitle = 'Sri Lanka Reis'
+  end
 end
