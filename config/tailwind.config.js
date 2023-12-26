0784d719e3450bf8063a1c0881a370b7f463f636
Martijn <m-j-heiden@hotmail.com>
2023-12-26 07:07:17 +0100

    Edit tailwind theme

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -17,21 +17,23 @@ module.exports = {
     },
   },
   daisyui: {
+    darkTheme: 'business',
     themes: [
-      "dark",
+      "corporate", // light theme // first one will be the default theme
+      "business", // dark theme
       {
-        mytheme: {
-          "base-100":  "#1d232a",
+      //   mytheme: {
+      //     "base-100":  "#1d232a",
 
-          "primary":   "#641ae6",
-          "secondary": "#d926a9",
-          "accent":    "#1fb2a6",
-          "neutral":   "#2a323c",
-          "info":      "#3abff8",
-          "success":   "#36d399",
-          "warning":   "#fbbd23",
-          "error":     "#f87272",
-        },
+      //     "primary":   "#641ae6",
+      //     "secondary": "#d926a9",
+      //     "accent":    "#1fb2a6",
+      //     "neutral":   "#2a323c",
+      //     "info":      "#3abff8",
+      //     "success":   "#36d399",
+      //     "warning":   "#fbbd23",
+      //     "error":     "#f87272",
+        // },
       },
     ],
   },
